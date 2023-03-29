986f844aed3c6b48aa74b3981acf98349dddea42
ArthurMeinhart <127998120+ArthurMeinhart@users.noreply.github.com>
2023-03-29 17:07:20 -0300

    Add files via upload

---
+++ aula01/calculadora.py
@@ -0,0 +1,15 @@
+a=float(input('Digite um número: '))  
+b=float(input('Digite outro número: '))    
+c=input('Escolha uma operação: ')
+
+if c=="*":
+    print(a*b)
+
+elif c=="/":
+    print(a/b)
+
+elif c=="+":
+    print(a+b)
+
+elif c=="-":
+    print(a-b)
