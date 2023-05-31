8d9b3ba2dcb21407137477f7dee85b77a0cbe6e2
ArthurMeinhart <127998120+ArthurMeinhart@users.noreply.github.com>
2023-05-31 18:56:04 -0300

    Add files via upload

---
+++ Atividade 31.05/Arquivo 2.py
@@ -0,0 +1,9 @@
+#Objetivo: Criar um programa que printe os numeros de uma lista e, depois, eles multiplicados por 2
+#Bonus: Colocar os numeros multiplicados em uma outra lista e printa-la
+
+
+lista=[2,3,7,12,2]
+
+for i in range(len(lista)):
+    entrada = lista[i]
+    print(entrada * 2)
---
+++ Atividade 31.05/Arquivo 3.py
@@ -0,0 +1,21 @@
+#Objetivo: Criar um programa que organize uma lista de nomes em ordem alfabetica
+#Bonus: Organizar por ordem alfabetica da ultima letra do nome
+
+# Programa 1:
+
+nome1 = input('Dê um nome para a lista: ')
+nome2 = input('Dê um nome para a lista: ')
+nome3 = input('Dê um nome para a lista: ')
+nome4 = input('Dê um nome para a lista: ')
+nome5 = input('Dê um nome para a lista: ')
+nome6 = input('Dê um nome para a lista: ')
+nomes = [nome1, nome2, nome3, nome4, nome5, nome6]
+alfabetica = sorted(nomes)
+print(alfabetica)
+
+
+# Programa 2: 
+
+#nomes = ["Arthur", "João", "Marcelo", "Antônia", "Sabrina", "Saraiva", "Régis", "Hector", "Juliana"]
+#alfabetica = sorted(nomes)
+#print(alfabetica)
